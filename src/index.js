677b5d04ce76047f08035a3d993e94978749d68b
Hassam-Ata <hassamata86@gmail.com>
2024-06-29 13:55:06 +0500

    added content in customers.html

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
-import './styles.css';
-import './customers/customers';
-import './orders/orders';
-import './bills/bills';
-import './managers/managers';
-import './waiters/waiters';
-import './chefs/chefs';
+//import './styles.css';
+// import './customers';
+// import './orders';
+// import './bills';
+// import './managers';
+// import './waiters';
+// import './chefs';
+const btn=document.querySelector('.customer-btn');
+const boxes=document.querySelector('.card-boxes');
+console.log(boxes);
+console.log(btn);
+btn.addEventListener('click',()=>{
+    console.log("button pressed");
+    window.location.href = "http://localhost:3000/src/customers/customers.html";
+    
+})
